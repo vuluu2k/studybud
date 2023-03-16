e0558f49e22a874b9db6ddc0d4a2a1a4f82b5aa0
vuluu <vuluu040320@gmail.com>
2023-03-16 22:18:01 +0700

    update

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ''
-    rooms = Room.objects.filter(topic__name=q)
+    rooms = Room.objects.filter(topic__name__icontains=q)
     topics = Topic.objects.all()
 
     context = {'rooms': rooms, 'topics': topics}
