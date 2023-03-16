5c545128aa0736e80294162089f079bb4327dc7b
vuluu <vuluu040320@gmail.com>
2023-03-16 10:45:08 +0700

    add config py

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Home</h1>")
+    print(request)
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse("<h1>Room</h1>")
+    print(request)
+    return render(request, 'room.html')
